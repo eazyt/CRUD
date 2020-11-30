f60a64e28566df6147011397bec3e501401b7019
eazyt <ramohlalen@gmail.com>
2020-11-30 16:53:48 +0200

    first commit - PATCH BY ID

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
+const _ = require('lodash')
 const mongoose = require('./db/mongoose')
 const  Todo = require('./models/todo')
 // const Use = require('../models/user')
@@ -73,6 +74,39 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
       res.status(400).send(err)
     })
+})
+
+app.patch('/todos/:id', (req, res) => { 
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) return res.status(400).send({
+    error: 'Invalid ID'
+  });
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else { 
+    body.completed = false,
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: 
+      body
+    
+  }, {
+    new: true
+  })
+    .then((todo) => {
+    if (!todo) return res.status(404).send()
+
+    res.send({
+      todo
+    })
+  }).catch((err) => {
+    res.status(400).send(err)
+  })
 })
 
 app.listen(PORT, () => {
